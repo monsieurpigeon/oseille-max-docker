dece4459d8e370bcd713b6d7c5450324dd21a219
Maxime PIGEON <maxpige@gmail.com>
2024-10-11 17:48:57 +0200

    add github action to build and push images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   });
 
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-dvh">
       <main className="bg-primary w-full h-full box-border border-8 border-yellow-200 flex items-center justify-center">
         <div className="flex flex-col">
           <Badge>{message}</Badge>
