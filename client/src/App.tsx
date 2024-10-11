61c3d235c8f10d75f274b14f2b61ddc2546d5314
Maxime PIGEON <maxpige@gmail.com>
2024-10-11 18:46:54 +0200

    fix build workflow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <main className="bg-primary w-full h-full box-border border-8 border-yellow-200 flex items-center justify-center">
         <div className="flex flex-col">
           <Badge>{message}</Badge>
-          <Button variant="secondary">Eat me</Button>
+          <Button variant="secondary">Drink me</Button>
         </div>
       </main>
     </div>
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes: Router = Router();
 
 routes.get("/", function (req, res) {
-  return res.json({ message: "Hello World" });
+  return res.json({ message: "Oseille Max" });
 });
 
 export default routes;
